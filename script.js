abb41bd2fab181770e0f2ed3016ebc9f61d60984
Rajat Choudhary <choudharyrajat037@gmail.com>
2023-03-24 20:09:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const hide = setTimeout(opacity, 7000);
+function opacity() {
+        document.getElementById("bg").style.display = "none";
+        document.getElementById("hero").style.display = "block";
+}
